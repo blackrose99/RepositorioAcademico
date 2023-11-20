f4dd617a98175635feeb7d37cae63a6ccd548f3e
Luis Manuel Castaño <111204814+blackrose99@users.noreply.github.com>
2023-11-20 11:08:47 -0500

    Cambios

--- src/Componentes/Estudiantes/PanelEstudiante.js
+++ src/Componentes/Estudiantes/PanelEstudiante.js
@@ -1,13 +1,84 @@
-// Componente del Panel del Estudiante (PanelEstudiante.js)
-import React from 'react';
+import React, { useEffect, useState } from "react";
+import EstudianteService from "../../servicios/EstudianteServices";
+import { useParams } from "react-router-dom"; // Importa useParams
+import { Link } from "react-router-dom";
 
-const PanelEstudiante = () => {
+import PuntajeComponente from "../calificaciones/Puntaje";
+import NivelComponente from "../calificaciones/Nivel";
+import "../calificaciones/califi.css";
+import imgestuInicio from "../../img/img-saberPro.png";
+
+const InfoEstudiante = () => {
+  const { id } = useParams();
+  console.log("ID obtenido:", id);
+  const [estudiante, setEstudiante] = useState(null);
+  useEffect(() => {
+    EstudianteService.getEstudianteById(id).then((response) => {
+      setEstudiante(response.data);
+    });
+  }, [id]);
+
+  if (!estudiante) {
+    return <div>Cargando informe...</div>;
+  }
+
   return (
-    <div>
-      <h1>Panel del Estudiante</h1>
-      {/* Contenido del panel del estudiante */}
+    <div className="student-info-container col-md-6">
+      <div className="container1">
+
+        <h3 className="name-nombre ">
+          {estudiante.primerNombre} {estudiante.segundoNombre} {estudiante.primerApellido} {estudiante.segundoApellido}{" "}
+         
+        </h3>
+        <img className="ml-5" src={imgestuInicio} width={200} height={100} />
+
+      </div>
+
+      <div className="container2 mb-4">
+  <div className="circle-puntaje">
+   <h4 className="mt-3"><PuntajeComponente estudianteId={id} /></h4>
+  </div>
+  <div className="circle-nivel">
+    <NivelComponente estudianteId={id} />
+  </div>
+
+</div>
+
+
+      <ul className="student-info-list">
+        <li>
+          <strong>Tipo de Documento:</strong> {estudiante.tipoDocumento}
+        </li>
+        <li>
+          <strong>Número de Documento:</strong> {estudiante.numeroDocumento}
+        </li>
+        <li>
+          <strong>Primer Apellido:</strong> {estudiante.primerApellido}
+        </li>
+        <li>
+          <strong>Segundo Apellido:</strong> {estudiante.segundoApellido}
+        </li>
+        <li>
+          <strong>Primer Nombre:</strong> {estudiante.primerNombre}
+        </li>
+        <li>
+          <strong>Segundo Nombre:</strong> {estudiante.segundoNombre}
+        </li>
+        <li>
+          <strong>Correo Electrónico:</strong> {estudiante.correoElectronico}
+        </li>
+        <li>
+          <strong>Número Telefónico:</strong> {estudiante.numeroTelefonico}
+        </li>
+        <li>
+          <strong>Número de Registro:</strong> {estudiante.numeroRegistro}
+        </li>
+      </ul>
+      <Link to={`/estudiantes/informe-calificaciones/${id}`}>
+        <button className="btn btn-info">Ver informe De Calificaciones</button>
+      </Link>
     </div>
   );
 };
 
-export default PanelEstudiante;
+export default InfoEstudiante;
--- src/Componentes/Login/Login.js
+++ src/Componentes/Login/Login.js
@@ -1,71 +1,67 @@
 import React, { useState } from 'react';
-import './Login.css'; // Importa el archivo CSS
-import Header from "../Globales/Header"
+import { useNavigate } from 'react-router-dom';
+import EstudianteService from '../../servicios/EstudianteServices';
+import NavigationBar from '../../plantillas/encabezado';
+import './EstudianteLoginForm.css'; // Importa tus estilos CSS
 
-import LoginImg from '../Globales/img_G/login.png';
-import Footer from "../Globales/Footer"
+function EstudianteLoginForm() {
+  const navigate = useNavigate();
 
-const InicioSesion = () => {
-  const [tipoUsuario, setTipoUsuario] = useState('estudiante');
-  const [mostrarFormularioDocente, setMostrarFormularioDocente] = useState(false);
+  const [formData, setFormData] = useState({
+    numeroDocumento: '',
+    numeroRegistro: '',
+    mensajeError: '',
+  });
 
-  const handleSubmit = (e) => {
+  const handleInputChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    // Lógica de autenticación
-  };
 
-  const cambiarTipoUsuario = (nuevoTipo) => {
-    if (nuevoTipo === 'docente') {
-      setMostrarFormularioDocente(true);
-    } else {
-      setMostrarFormularioDocente(false);
+    const { numeroDocumento, numeroRegistro } = formData;
+
+    try {
+      const estudianteId = await EstudianteService.loginEstudiante(numeroDocumento, numeroRegistro);
+      navigate(`/estudiantes/${estudianteId}`);
+    } catch (error) {
+      setFormData({ ...formData, mensajeError: error.message });
     }
-    setTipoUsuario(nuevoTipo);
   };
-
-  const cardClass = tipoUsuario === 'estudiante' ? 'cardLogin estudiante' : 'cardLogin docente';
 
   return (
     <div>
-      <div>
-      <h2>Login</h2>
-      <label>
-        <input
-          type="radio"
-          name="userType"
-          value="student"
-          checked={isStudent}
-          onChange={() => setIsStudent(true)}
-        />
-        Estudiante
-      </label>
-      <label>
-        <input
-          type="radio"
-          name="userType"
-          value="teacher"
-          checked={!isStudent}
-          onChange={() => setIsStudent(false)}
-        />
-        Docente
-      </label>
-      {transitions.map(({ item, key, props }) =>
-        item ? (
-          <animated.div key={key} style={props}>
-            {/* Contenido para estudiantes */}
-            <p>Contenido para estudiantes</p>
-          </animated.div>
-        ) : (
-          <animated.div key={key} style={props}>
-            {/* Contenido para docentes */}
-            <p>Contenido para docentes</p>
-          </animated.div>
-        )
-      )}
-    </div>
-    <Footer/>
+      <NavigationBar />
+      <div className="estudiante-form p-5 col-md-6">
+        <h2 className="form-title">Iniciar Sesión como Estudiante</h2>
+        <form onSubmit={handleSubmit}>
+          <div className="mb-3">
+            <label htmlFor="numeroDocumento" className="form-label">Número de Documento:</label>
+            <input
+              type="text"
+              name="numeroDocumento"
+              value={formData.numeroDocumento}
+              onChange={handleInputChange}
+              className="form-control"
+            />
+          </div>
+          <div className="mb-3">
+            <label htmlFor="numeroRegistro" className="form-label">Número de Registro:</label>
+            <input
+              type="text"
+              name="numeroRegistro"
+              value={formData.numeroRegistro}
+              onChange={handleInputChange}
+              className="form-control"
+            />
+          </div>
+          <button type="submit" className="btn btn-primary">Iniciar Sesión</button>
+        </form>
+        <p className="text-danger my-3">{formData.mensajeError}</p>
+      </div>
     </div>
   );
-};
+}
 
-export default InicioSesion;
+export default EstudianteLoginForm;
