6a48cc2420ecaec4acf5bd9f58e96e8387bc57d3
Luis Manuel Castaño <111204814+blackrose99@users.noreply.github.com>
2023-11-24 12:35:56 -0500

    footer

--- src/Componentes/Globales/Footer.js
+++ src/Componentes/Globales/Footer.js
@@ -9,12 +9,10 @@ const Footer = () => {
           <div className="col-md-6">
             <h5>Información de Contacto</h5>
             <p>Email: info@academic-hub.com</p>
-            <p>Teléfono: +123456789</p>
           </div>
           <div className="col-md-6">
             <h5>Enlaces Rápidos</h5>
             <ul>
-              <li><a href="/about">Acerca de nosotros</a></li>
               <li><a href="/terms">Términos y Condiciones</a></li>
               <li><a href="/privacy">Política de Privacidad</a></li>
             </ul>
