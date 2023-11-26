efe63f3490525fce529096b75ba506aaa6996692
Luis Manuel Castaño <111204814+blackrose99@users.noreply.github.com>
2023-11-26 18:01:18 -0500

    header principal

--- src/Componentes/Globales/Header.js
+++ src/Componentes/Globales/Header.js
@@ -1,22 +1,24 @@
 import React from 'react';
-import { Link } from 'react-router-dom'; // Importa Link desde react-router-dom
-import './Header.css'; // Importa el archivo de estilos del Header
-import logo from './img_G/logo-removebg-preview.png'; // Importa la imagen del logotipo
+import { Link } from 'react-router-dom';
+import './Header.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHome, faUser, faChalkboardTeacher } from '@fortawesome/free-solid-svg-icons';
+
+import logo from './img_G/logo-removebg-preview.png';
 
 const Header = () => {
   return (
     <header className="header">
       <div className="containerHeader">
         <div className="logo">
-          <img src={logo} alt="Logo de la aplicación"  />
-         <span className='tituloHeader'>Repositorio Académico</span>
+          <img src={logo} alt="Logo de la aplicación" />
+          <span className='tituloHeader'>Repositorio Académico</span>
         </div>
         <nav>
           <ul>
-            <li><Link to="/">Inicio</Link></li>
-            <li><Link to="/login-estudiante">Login Estudiante</Link></li>
-            <li><Link to="/login-docente">Login Docentes</Link></li>
-
+            <li><Link to="/"><FontAwesomeIcon icon={faHome} /> Inicio</Link></li>
+            <li><Link to="/login-estudiante"><FontAwesomeIcon icon={faUser} /> Login Estudiante</Link></li>
+            <li><Link to="/login-docente"><FontAwesomeIcon icon={faChalkboardTeacher} /> Login Docentes</Link></li>
           </ul>
         </nav>
       </div>
