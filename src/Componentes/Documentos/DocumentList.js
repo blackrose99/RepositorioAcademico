eae9419fe76b0cd1f0ef745bb4943450389d00d8
Luis Manuel Castaño <111204814+blackrose99@users.noreply.github.com>
2023-11-26 17:02:53 -0500

    VerDocumento

--- src/Componentes/Documentos/DocumentList.js
+++ src/Componentes/Documentos/DocumentList.js
@@ -27,14 +27,14 @@ const ListaDocumentos = () => {
     fetchDocumentos();
   }, [docenteId]);
 
-  const truncateDescription = (description, maxLength) => {
-    // Asegúrate de que 'description' no sea null antes de acceder a 'length'
-    if (description && description.length > maxLength) {
-      return `${description.substring(0, maxLength)}...`;
-    }
-    return description;
-  };
-  
+ const truncateDescription = (description, maxLength) => {
+  // Asegúrate de que 'description' no sea null antes de acceder a 'length'
+  if (description && description.length > maxLength) {
+    return `${description.substring(0, maxLength)}...`;
+  }
+  return description;
+};
+
 
   const handleSearchChange = (e) => {
     const searchText = e.target.value.toLowerCase();
--- src/Componentes/Documentos/VerDocumento.js
+++ src/Componentes/Documentos/VerDocumento.js
@@ -1,32 +1,31 @@
 import React, { useState, useEffect } from 'react';
 import DocentesService from '../../Services/DocenteServices';
 import { useParams } from 'react-router-dom';
+import "./VerDocumento.css";
+import Modal from 'react-modal';
+import { RingLoader } from 'react-spinners';
 
-function base64ToFile(base64String, fileName) {
-  const byteCharacters = atob(base64String);
-  const byteNumbers = new Array(byteCharacters.length);
-  for (let i = 0; i < byteCharacters.length; i++) {
-    byteNumbers[i] = byteCharacters.charCodeAt(i);
-  }
-  const byteArray = new Uint8Array(byteNumbers);
-  const blob = new Blob([byteArray], { type: 'application/pdf' });
-  const file = new File([blob], fileName, { type: 'application/pdf' });
-  return file;
-}
+Modal.setAppElement('#root');
 
 const VerDocumento = () => {
   const { id: documentoId } = useParams();
   const [documento, setDocumento] = useState(null);
+  const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
+  const [modalIsOpen, setModalIsOpen] = useState(true);
 
   useEffect(() => {
     const fetchDocumento = async () => {
       try {
         const response = await DocentesService.getDocumentById(documentoId);
         setDocumento(response.data);
+        setLoading(false); // Se establece como falso cuando el documento se carga
+        setModalIsOpen(false); // Cerrar el modal cuando el documento se carga
+
       } catch (error) {
         console.error('Error al obtener el documento:', error);
         setError(error);
+        setLoading(false); // También establece como falso en caso de error
       }
     };
 
@@ -34,9 +33,9 @@ const VerDocumento = () => {
   }, [documentoId]);
 
   const handleDownload = async () => {
-    const base64String = documento.archivo; 
+    const base64String = documento.archivo;
     console.log('Valor de documento.archivo:', base64String);
-  
+
     try {
       const decodedData = atob(base64String);
       const byteCharacters = new Uint8Array(decodedData.length);
@@ -44,47 +43,62 @@ const VerDocumento = () => {
         byteCharacters[i] = decodedData.charCodeAt(i);
       }
       const blob = new Blob([byteCharacters], { type: 'application/pdf' });
-  
+
       const pdfUrl = URL.createObjectURL(blob);
       window.open(pdfUrl, '_blank');
     } catch (error) {
       console.error('Error al decodificar y mostrar el archivo PDF:', error);
     }
   };
-  
-
 
   if (error) {
     return <div>Error al obtener el documento: {error.message}</div>;
   }
 
-  if (!documento) {
-    return <div>Cargando...</div>;
+ if (loading) {
+    return (
+      <Modal
+        isOpen={modalIsOpen}
+        onRequestClose={() => setModalIsOpen(false)}
+        className="modal-content"
+        overlayClassName="modal-overlay"
+      >
+        <RingLoader color="#36D7B7" loading={loading} size={150} />
+        <div>Cargando...</div>
+      </Modal>
+    );
   }
 
+
+  if (!documento) {
+    return <div>No se encontró el documento.</div>;
+  }
   return (
-    <div className='container'>
-      <h2>Detalles del Documento</h2>
-      <div>
+    <div className='ver-documento-container'>
+      <h2 className='ver-documento-title'>Detalles del Documento</h2>
+      <div className='ver-documento-item'>
         <strong>Nombre:</strong> {documento.nombre}
       </div>
-      <div>
+      <div className='ver-documento-item'>
         <strong>Categoría:</strong> {documento.categoria}
       </div>
-      <div>
+      <div className='ver-documento-item'>
         <strong>Autor:</strong> {documento.autor}
       </div>
-      <div>
+      <div className='ver-documento-item'>
         <strong>Descripción:</strong> {documento.descripcion}
       </div>
-      <div>
+      <div className='ver-documento-item'>
         <strong>Fecha de Publicación:</strong> {documento.fechaPublicacion}
       </div>
-      <div>
-        <strong>Publicad Por El Docente:</strong> {documento.docentes.map(docente => docente.primerNombre + ' ' + docente.primerApellido).join(', ')}
+      <div className='ver-documento-item'>
+        <strong>Publicado Por El Docente:</strong>{' '}
+        {documento.docentes.map((docente) => docente.primerNombre + ' ' + docente.primerApellido).join(', ')}
       </div>
-      <div>
-        <button onClick={handleDownload}>Descargar Archivo</button>
+      <div className='ver-documento-item'>
+        <button className='ver-documento-button' onClick={handleDownload}>
+          Descargar Archivo
+        </button>
       </div>
     </div>
   );
